11e9acc493d2615001d07fb433190ec73e4f160f
phaasenritter <peer.haasenritter@hotmail.com>
2024-04-26 12:15:05 +0200

    Neuer Code für die die Sortieralgorithmen

--- Ex01_HaasenritterFeldmannRockenstein_project/src/main/java/de/unistuttgart/dsass2024/ex01/p5/Sorter.java
+++ Ex01_HaasenritterFeldmannRockenstein_project/src/main/java/de/unistuttgart/dsass2024/ex01/p5/Sorter.java
@@ -11,6 +11,18 @@ public class Sorter {
      *             in the end
      */
     public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
+        int length = list.getSize();
+
+        for(int i = 0; i < length ; i++){
+            int maxPos = i;
+
+            for(int j = i + 1; j < length; j++ ){
+                if(list.getElement(j).compareTo(list.getElement(maxPos))>0){
+                    maxPos = j;
+                }
+            }
+            list.swapElements(i, maxPos);
+        }
 
     }
 
@@ -23,6 +35,15 @@ public class Sorter {
      *             in the end
      */
     public static <T extends Comparable<T>> void insertionSort(ISimpleList<T> list) {
+        int length = list.getSize();
+        for(int i = 1; i < length; i++){
+            T elementToSort = list.getElement(i);
+            int j = i - 1;
+            while ((j > -1)&& (list.getElement(j).compareTo(elementToSort) == 1)){
+                list.swapElements(j+1,j);
+            }
+        }
+
 
     }
 
@@ -35,6 +56,14 @@ public class Sorter {
      *             in the end
      */
     public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
+        int length = list.getSize();
+        for(int i = 1; i < length; i++){
+            for(int j = 0; j < length - 1; j++){
+                if(list.getElement(j).compareTo(list.getElement(j+1)) == 1){
+                    list.swapElements(j,j+1);
+                }
+            }
+        }
 
     }
 }
