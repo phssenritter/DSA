7afc64ed0f9fbe472b22ffd34c7613e4344b91d7
phssenritter <148816650+phssenritter@users.noreply.github.com>
2024-04-26 10:20:40 +0200

    Add files via upload

---
+++ Ex01_LastNamesOfMembers_project/src/test/java/de/unistuttgart/dsass2024/ex01/p5/SorterTest.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.dsass2024.ex01.p5;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class SorterTest {
+
+}
