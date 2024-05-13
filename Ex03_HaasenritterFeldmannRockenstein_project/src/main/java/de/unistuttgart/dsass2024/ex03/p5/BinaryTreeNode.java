e8d02a4338a0e8db9cc63782b8133cada4457df1
phaasenritter <peer.haasenritter@hotmail.com>
2024-05-13 10:09:37 +0200

    Teilaufgabe a)

--- Ex03_HaasenritterFeldmannRockenstein_project/src/main/java/de/unistuttgart/dsass2024/ex03/p5/BinaryTreeNode.java
+++ Ex03_HaasenritterFeldmannRockenstein_project/src/main/java/de/unistuttgart/dsass2024/ex03/p5/BinaryTreeNode.java
@@ -2,10 +2,37 @@ package de.unistuttgart.dsass2024.ex03.p5;
 
 public class BinaryTreeNode<T extends Comparable<T>> implements IBinaryTreeNode<T> {
 
+    private T value;
+    private IBinaryTreeNode<T> leftChild;
+    private IBinaryTreeNode<T> rightChild;
+
+
 
     public BinaryTreeNode() {
+        this.value = null;
+        this.leftChild = null;
+        this.rightChild = null;
 
     }
 
+    public void setValue(T val){
+        this.value = val;
+    }
 
+    public T getValue() {
+        return this.value;
+    }
+
+    public void setLeftChild(IBinaryTreeNode<T> left){
+        this.leftChild = left;
+
+    }
+
+    public IBinaryTreeNode<T> getLeftChild() {
+        return leftChild;
+    }
+
+    public void setRightChild(IBinaryTreeNode<T> right) {
+        this.rightChild = right;
+    }
 }
